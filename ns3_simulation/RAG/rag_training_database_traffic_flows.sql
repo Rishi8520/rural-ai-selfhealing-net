0dfdc6e7499434d3ef1cd8941e1fe667f94d21e5
Rishi8520 <rishiraj.mitblr2022@learner.manipal.edu>
2025-06-13 04:57:36 +0530

    Added NS3 Network database

---
+++ ns3_simulation/RAG/rag_training_database_traffic_flows.sql
@@ -0,0 +1,17 @@
+-- TRAFFIC FLOWS TABLE
+INSERT INTO TrafficFlows VALUES ('flow_CORE-0_to_ACC-0_0', 'CORE-0', 'ACC-0', 22, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-0_to_ACC-10_0', 'CORE-0', 'ACC-10', 10, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-0_to_ACC-20_0', 'CORE-0', 'ACC-20', 15, 'HTTP', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-1_to_ACC-0_0', 'CORE-1', 'ACC-0', 29, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-1_to_ACC-10_0', 'CORE-1', 'ACC-10', 21, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-1_to_ACC-20_0', 'CORE-1', 'ACC-20', 21, 'VoIP', 'high', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-2_to_ACC-0_0', 'CORE-2', 'ACC-0', 25, 'VoIP', 'high', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-2_to_ACC-10_0', 'CORE-2', 'ACC-10', 17, 'VoIP', 'high', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-2_to_ACC-20_0', 'CORE-2', 'ACC-20', 26, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-3_to_ACC-0_0', 'CORE-3', 'ACC-0', 16, 'HTTP', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-3_to_ACC-10_0', 'CORE-3', 'ACC-10', 13, 'HTTP', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-3_to_ACC-20_0', 'CORE-3', 'ACC-20', 18, 'VoIP', 'high', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-4_to_ACC-0_0', 'CORE-4', 'ACC-0', 22, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-4_to_ACC-10_0', 'CORE-4', 'ACC-10', 29, 'VoIP', 'high', '0.000000', 'NULL', '2495.000000');
+INSERT INTO TrafficFlows VALUES ('flow_CORE-4_to_ACC-20_0', 'CORE-4', 'ACC-20', 18, 'Database', 'medium', '0.000000', 'NULL', '2495.000000');
+
